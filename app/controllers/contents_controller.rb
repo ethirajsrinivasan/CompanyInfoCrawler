6da2676c367af2eff303a54653d897ee96ce0a6b
ethiraj <ethirajsrinivasan@gmail.com>
2018-05-04 19:03:56 +0530

    search done

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,9 @@
 class ContentsController < ApplicationController
+
+	def index
+	end
+
+	def search
+		@contents = Content.search params["query"], where: {company_id: params[:company_id].to_i}
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ mount Sidekiq::Web => '/sidekiq'
   		put :scrap_content
   	end
   end
+
+  resources :contents do
+  	collection do
+  		get :search
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
